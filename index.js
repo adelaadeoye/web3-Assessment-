1531e9988d5a814367a70ce873e6efd16376c6ee
adelaadeoye <adeoye.adela@gmail.com>
2021-03-09 21:05:56 +0100

    Thank you

---
+++ index.js
@@ -0,0 +1,11 @@
+const basicCal = (a, b) => {
+  console.log(
+    `The Multiplication of ${a} and ${b} = ${a * b}\nThe Addition of ${a} and ${b} = ${
+      a + b
+    }\nThe Subtraction addition of ${a} and ${b} = ${
+        a - b
+      }\nThe Division addition of ${a} and ${b} = ${a/b}\n`
+  );
+};
+
+basicCal(2, 4);
